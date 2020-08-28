72aa2c258bb9d3665d0e2b3f185017c887bc46f3
Rondineleg <rondineleg@edu.univali.br>
2020-08-28 09:41:10 -0300

    adicionando referencia a linq

--- BlazorSignalRApp/Server/Startup.cs
+++ BlazorSignalRApp/Server/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorSignalRApp.Server.Hubs;
+using System.Linq;
 
 namespace BlazorSignalRApp.Server
 {
