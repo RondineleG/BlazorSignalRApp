720431cd24a4baea4bc70dacecd5416e7e2708d5
Rondineleg <rondineleg@edu.univali.br>
2020-08-28 09:40:33 -0300

    adicionando suporte a SignalR

---
+++ BlazorSignalRApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.VisualBasic;
+using System.Threading.Tasks;
+
+namespace BlazorSignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMesage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user,message);
+        }
+    }
+}
--- BlazorSignalRApp/Server/Startup.cs
+++ BlazorSignalRApp/Server/Startup.cs
@@ -1,11 +1,10 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Linq;
+using BlazorSignalRApp.Server.Hubs;
 
 namespace BlazorSignalRApp.Server
 {
@@ -22,14 +21,21 @@ namespace BlazorSignalRApp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSignalR();
             services.AddControllersWithViews();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" });
+            });
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
